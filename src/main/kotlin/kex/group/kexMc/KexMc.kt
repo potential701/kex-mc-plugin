34e99d6027fbe2e8b4e4f348d1950a6ae819965b
Viktors Makreckis <vmakreckis2809@gmail.com>
2025-03-22 12:38:50 +0000

    Register landmark Command in KexMc.kt

--- src/main/kotlin/kex/group/kexMc/KexMc.kt
+++ src/main/kotlin/kex/group/kexMc/KexMc.kt
@@ -2,6 +2,7 @@ package kex.group.kexMc
 
 import kex.group.kexMc.commands.AllowJoinCommand
 import kex.group.kexMc.commands.DisallowJoinCommand
+import kex.group.kexMc.commands.LandmarkCommand
 import kex.group.kexMc.listeners.PlayerJoinListener
 import kex.group.kexMc.util.Config
 import org.bukkit.plugin.java.JavaPlugin
@@ -9,28 +10,21 @@ import org.bukkit.plugin.java.JavaPlugin
 class KexMc : JavaPlugin() {
 
     override fun onEnable() {
-        // Plugin startup logic
+//        Plugin startup logic
         logger.info("KEX MC is enabled.")
 
-        createAllowedPlayersConfig()
+        val config = Config(this, "config.yml")
+        config.save()
 
         PlayerJoinListener(this)
 
         getCommand("allow_join")?.setExecutor(AllowJoinCommand(this))
         getCommand("disallow_join")?.setExecutor(DisallowJoinCommand(this))
+        getCommand("landmark")?.setExecutor(LandmarkCommand(this))
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
+//        Plugin shutdown logic
         logger.info("KEX MC is disabled.")
-    }
-
-    private fun createAllowedPlayersConfig(){
-        val config = Config(this, "config.yml")
-        val allowedPlayers = config.getConfig().getList("allowed_players")
-        if(allowedPlayers?.isEmpty() == false) return
-
-        config.getConfig().set("allowed_players", listOf<String>())
-        config.save()
     }
 }
